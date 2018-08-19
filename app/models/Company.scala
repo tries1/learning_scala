6f937f39707a814a07c1ef658ad149814b4a107a
glenn <glenn.jo@kakaocorp.com>
2018-08-19 20:45:41 +0900

    add Company

--- app/models/Company.scala
+++ app/models/Company.scala
@@ -1,3 +1,12 @@
 package models
 
+import anorm.SqlParser._
+
 case class Company (id: Option[Long] = None, name: String)
+
+object Company {
+  val simple = {
+    get[Option[Long]]("company.id") ~
+    get[String]("company.name") map { }
+  }
+}
--- app/models/Computer.scala
+++ app/models/Computer.scala
@@ -1,5 +1,23 @@
 package models
 
 import java.util.Date
+import anorm.SqlParser._
 
 case class Computer (id: Option[Long] = None, name: String, introduced: Option[Date], discontinued: Option[Date], companyId: Option[Long])
+
+object Computer {
+  val simple = {
+    get[Option[Long]]("computer.id") ~
+    get[String]("computer.name") ~
+    get[Option[Date]]("computer.introduced") ~
+    get[Option[Date]]("computer.discontinued") ~
+    get[Option[Long]]("computer.company_id") map {
+      case id~name~introduced~discontinued~companyId =>
+        Computer(id, name, introduced, discontinued, companyId)
+    }
+  }
+
+  val withCompany = Computer.simple ~ (Company.simple ?) map {
+    case computer~company => (computer, company)
+  }
+}
